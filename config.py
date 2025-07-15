2c0ea7cd89a76f12c335c529796a5fa77ce9e491
Devil7372 <devilgamingofficial59@gmail.com>
2025-07-15 12:59:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+API_ID = int(os.getenv("API_ID", "21185801"))
+API_HASH = os.getenv("API_HASH", "4235ef431f138309cb9f56ae179a24ba")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7581945436:AAHhX7Msz_MnmPiZknn6ejbQbxZVzDrPQVw")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "7057341064").split(",")))
